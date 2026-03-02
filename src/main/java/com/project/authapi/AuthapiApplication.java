bdc80e12e22cd349f43930ac2498cf76d34d883c
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-03-02 22:54:22 +0530

    feat(app): scaffold AuthapiApplication.java

---
+++ src/main/java/com/project/authapi/AuthapiApplication.java
@@ -0,0 +1,7 @@
+package com.project.authapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
