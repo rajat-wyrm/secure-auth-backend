f8d7c3158fa4a5c7d0a73580a1409342aa9b8f14
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-03-03 13:49:29 +0530

    feat(app): complete AuthapiApplication.java

--- src/main/java/com/project/authapi/AuthapiApplication.java
+++ src/main/java/com/project/authapi/AuthapiApplication.java
@@ -5,3 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.project.authapi")
+public class AuthapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthapiApplication.class, args);
+    }
+}
