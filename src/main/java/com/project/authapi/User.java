58693ed10e5a7602baee52d2223db3748cfd3eb4
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-03-11 21:56:01 +0530

    feat(model): extend User.java (chunk 2/3)

--- src/main/java/com/project/authapi/User.java
+++ src/main/java/com/project/authapi/User.java
@@ -12,3 +12,17 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "users")
 public class User {
 
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(min = 8, max = 128)
+    private String password;
+
+    public User() {}
+
