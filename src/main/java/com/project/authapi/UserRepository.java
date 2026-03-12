bf77effa2731d8023060829553ea6dd1b9ccd544
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-03-12 17:41:41 +0530

    feat(model): complete UserRepository.java

--- src/main/java/com/project/authapi/UserRepository.java
+++ src/main/java/com/project/authapi/UserRepository.java
@@ -3,3 +3,9 @@ package com.project.authapi;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
