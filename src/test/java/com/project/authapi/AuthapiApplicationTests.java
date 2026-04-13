836bbe0ad41fa210323e9877f798ed9fa94de7df
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-04-13 10:40:26 +0530

    test(test): scaffold AuthapiApplicationTests.java

---
+++ src/test/java/com/project/authapi/AuthapiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.project.authapi;
+
+import com.project.authapi.security.JwtService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AuthapiApplicationTests {
