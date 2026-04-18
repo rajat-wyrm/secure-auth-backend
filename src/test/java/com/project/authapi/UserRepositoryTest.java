6cef066d5dc5863a52c878011094a1dd7ff0ec83
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-04-18 13:34:46 +0530

    test(test): complete UserRepositoryTest.java

--- src/test/java/com/project/authapi/UserRepositoryTest.java
+++ src/test/java/com/project/authapi/UserRepositoryTest.java
@@ -28,3 +28,19 @@ class UserRepositoryTest {
         User u = new User("dave", passwordEncoder.encode("DavePass123!"));
         userRepository.save(u);
 
+        Optional<User> found = userRepository.findByUsername("dave");
+        assertThat(found).isPresent();
+        assertThat(passwordEncoder.matches("DavePass123!", found.get().getPassword())).isTrue();
+    }
+
+    @Test
+    void exists_by_username_returns_true_for_existing_user() {
+        userRepository.save(new User("erin", passwordEncoder.encode("ErinPass123!")));
+        assertThat(userRepository.existsByUsername("erin")).isTrue();
+    }
+
+    @Test
+    void find_by_username_returns_empty_for_missing() {
+        assertThat(userRepository.findByUsername("nobody")).isEmpty();
+    }
+}
