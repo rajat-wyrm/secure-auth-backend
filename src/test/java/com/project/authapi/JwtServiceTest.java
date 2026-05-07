6370c276fc55704fbd7e0d447e7007efdd577197
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-05-07 13:08:24 +0530

    test(test): scaffold JwtServiceTest.java

---
+++ src/test/java/com/project/authapi/JwtServiceTest.java
@@ -0,0 +1,15 @@
+package com.project.authapi;
+
+import com.project.authapi.security.JwtService;
+import io.jsonwebtoken.Claims;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class JwtServiceTest {
