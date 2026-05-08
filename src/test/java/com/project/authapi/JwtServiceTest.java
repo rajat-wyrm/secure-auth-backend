5e9689492d6a4a867b64c16a3bb921234b19947c
rajat-wyrm <rajat-wyrm@users.noreply.github.com>
2026-05-08 08:22:51 +0530

    test(test): complete JwtServiceTest.java

--- src/test/java/com/project/authapi/JwtServiceTest.java
+++ src/test/java/com/project/authapi/JwtServiceTest.java
@@ -13,3 +13,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @SpringBootTest
 @ActiveProfiles("test")
 class JwtServiceTest {
+
+    @Autowired JwtService jwtService;
+
+    @Test
+    void generate_then_parse_returns_same_userId() {
+        String token = jwtService.generateToken("user-abc-123");
+        Claims claims = jwtService.parseAndValidate(token);
+        assertThat(claims.getSubject()).isEqualTo("user-abc-123");
+    }
+
+    @Test
+    void parse_throws_for_garbage_token() {
+        assertThatThrownBy(() -> jwtService.parseAndValidate("not.a.token"))
+                .isInstanceOf(io.jsonwebtoken.JwtException.class);
+    }
+}
